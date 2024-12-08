a287669bccfcfa03e4883d5d31e64e046b40505d
Joshua <joshuahawi24@gmail.com>
2024-12-08 15:20:28 +0300

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const PORT = process.env.PORT || 3030;
 
-
+app.listen(PORT, () => {
+  console.log(` 🥦 JH's Infosec server started on port ${PORT}`);
+});
 
 
 
