14fdeb24e15b4554c311345daecf2bff05b2c38e
Joshua <joshuahawi24@gmail.com>
2024-12-08 17:39:23 +0300

    Updated app

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const PORT = process.env.PORT || 3030;
+
 
 
 
@@ -57,5 +57,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(` 🥦 JH's Infosec server started on port ${PORT}`);
+  console.log(` 🥦 JH's Infosec server started on port ${port}`);
 });
