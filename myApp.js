ea87eb6771a51ca7c342b46257660379e2a72295
Joshua <joshuahawi24@gmail.com>
2024-12-09 16:57:33 +0300

    Sniffer

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}))
 app.use(helmet.frameguard({action: 'deny'}))
 app.use(helmet.xssFilter())
-
+app.use(helmet.noSniff())
 
 
 
