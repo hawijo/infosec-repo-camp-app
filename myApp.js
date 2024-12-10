d6f7011c01e3e2b37ce1533b2ce5703b9fd5d9bc
Joshua <joshuahawi24@gmail.com>
2024-12-10 11:41:37 +0300

    Updated app

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
-ninetyDaysInSeconds = 90*24*60*60;
+ninetyDaysInSeconds = 90*24*60*60*100;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
